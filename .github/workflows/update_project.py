ca76fd3a62da6204d3f4c3963aa5ad530406701d
Kaikkitietokoneista <info@kaikkitietokoneista.net>
2021-10-14 17:34:51 +0300

    Update projects from gh event api

--- .github/workflows/update_project.py
+++ .github/workflows/update_project.py
@@ -1,20 +1,14 @@
 import requests
 import re
-from datetime import datetime, time
 
 f = open("README.md", "r")
 file_content = f.read()
 
-projects = requests.get("https://api.github.com/users/kaikkitietokoneista/repos").json()
+events = requests.get("https://api.github.com/users/kaikkitietokoneista/events").json()
 
-biggest_time = datetime.strptime("1999-09-27T17:51:22Z", '%Y-%m-%dT%H:%M:%SZ')
-
-for project in projects:
-    time_formatted = datetime.strptime(project["pushed_at"], '%Y-%m-%dT%H:%M:%SZ')
-    if time_formatted > biggest_time and project["name"] != "kaikkitietokoneista":
-        biggest_time = time_formatted
-        project_name = project["name"]
-        project_url = project["html_url"]
+github_user = events[0]["actor"]["login"]
+project_name = events[0]["repo"]["name"].replace(github_user + "/", "", 1)
+project_url = "https://github.com/" + events[0]["repo"]["name"]
 
 new_content = re.sub(r'currently working on .*', 'currently working on [' + project_name + '](' + project_url + ')', file_content)
 
